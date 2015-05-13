05a3e67be06530c38d21921ebcc97d42fbca315f
Thomas Khaipi <khaipi@gmail.com>
2015-05-13 18:04:10 +0200

    add destroy methode

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,16 +8,41 @@ class ArticlesController < ApplicationController
     	@article = Article.find(params[:id])
   	end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
 	def new
 		@article = Article.new
 	end
 
 	def create
         @article = Article.new(article_params)
-        Rails.logger.debug('before save')
-        @article.save
-        Rails.logger.debug('after save')
-        redirect_to @article
+        #Rails.logger.debug('before save')
+        
+        if @article.save
+        #Rails.logger.debug('after save')
+        	redirect_to @article
+    	else
+    		render 'new'
+    	end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
 
 	private
